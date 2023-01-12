14758cf0a8ec6bb767e63bfd05c2ab55569fc079
Emelie <mli_ema@yahoo.se>
2023-01-12 12:43:52 +0100

    check test in vsc

--- pytest/test_saucedemo.py
+++ pytest/test_saucedemo.py
@@ -1,14 +1,14 @@
 from playwright.sync_api import Page
 import pytest
 
+
 #  @pytest.mark.skip_browser("chromium")
 @pytest.mark.only_browser("chromium")
 def test_title(page: Page):
     page.goto("https://www.saucedemo.com/")
     assert page.title() == "Swag Labs"
 
-'''
+
 def test_inventory_site(page: Page):
-    page.goto("/inventory.html")
+    page.goto("https://saucedemo.com/inventory.html")
     assert page.inner_text('h3') == "Epic sadface: You can access '/inventory.html'"
-'''
